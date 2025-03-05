c1726ce2076fb3194c1df645a2e28a1b1beafac8
shivu1919 <shivamthakur.ss129@gmail.com>
2025-03-05 20:27:54 +0530

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,10 @@ import style from '../css/home.module.css'
 function Home() {
 
     const[fromCity, setFromCity] = useState('')
+    const[toCity, setToCity] = useState([])
+    const[source, setSource] = useState('Hyderabad')
+    const[destination, setDestination] = useState('')
+    const[date, setDate] = useState('')
 
     const fetchFromCity = () =>{
         fetch("http://localhost:8080/from-city")
@@ -17,26 +21,72 @@ function Home() {
         fetch("http://localhost:8080/to-city")
         .then((response)=>response.json())
         .then((data)=>{
-            console.log(data)
+           
+            setToCity(data)
+            console.log(toCity)
         })
     }
 
+    const searchTicket = () =>{
+        if(destination=="Guntur"){
+            fetch("http://localhost:8080/bookToGtr")
+            .then((response)=>response.text())
+            .then((data)=>{
+               alert(data)
+            })
+        }
+        else if(destination=="Vizag"){
+            fetch("http://localhost:8080/bookToVzg")
+            .then((response)=>response.text())
+            .then((data)=>{
+               alert(data)
+            })
+        }
+        else if(destination=="Bengluru"){
+            fetch("http://localhost:8080/bookToBlr")
+            .then((response)=>response.text())
+            .then((data)=>{
+               alert(data)
+            })
+        }
+    }
+
     useEffect(fetchFromCity,[])
+    useEffect(fetchToCity,[])
 
 
   return (
    <>
         <div className={style.container}>
             <h1 className={style.heading}>Book My Ride</h1>
+
+            <div className={style.cities}>
             <div>
-                <label>From</label>
-                <br/>
-                <select>
-                    <option>{fromCity}</option>
-                </select>
+                    <label>From</label>
+                    <br/>
+                    <select>
+                        <option>{fromCity}</option>
+                    </select>
             </div>
             
-            <button onClick={fetchToCity}>ToCity</button>
+        
+                <div>
+                    <label>To</label>  
+                    <br/>  
+                    <select onChange={(e)=>setDestination(e.target.value)}>
+                        <option value="">Select your destination</option>
+                        {toCity.map((city,index)=><option key={index}>{city.name}</option>)}
+                    </select>
+                </div> 
+
+                <div>
+                    <br/>
+                    <input type="date" onChange={(e)=>setDate(e.target.value)}></input>
+                </div>
+            </div>
+
+            <button onClick={searchTicket}>Search</button>
+           
         </div>   
 
 
